f17fcd20ac57039a1c588aa4863c19de50736cb2
Youness Ahasla <unesseah56@gmail.com>
2022-06-07 20:21:04 +0100

    temprature commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//this is the forcast for today
+const kelvin = 0;
+//converting kelvin to celsius
+const celsius = kelvin - 273;
+//convert fahrenheit to celsius and make sure the the result is an iteger suing .floor
+const fahrenheit = Math.floor(celsius * (9/5) + 32);
+//console log and use string interpolation
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
+
+//change the temprature in line 2 to get that result in fahrenheit
+
+// const newton = Math.floor(celsius * (33/100));
+// console.log(newton)
